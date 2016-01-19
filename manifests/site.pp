0e9abe773b58c95ccd3d127434aed583a5d3cb9a
Adriaan Peeters <apeeters@lashout.net>
2016-01-19 12:14:00 +0100

    Add upstream ruby versions

--- manifests/site.pp
+++ manifests/site.pp
@@ -70,8 +70,12 @@ node default {
 
   # default ruby versions
   ruby::version { '1.8.7-p358': }
+  ruby::version { '1.9.3': }
+  ruby::version { '2.0.0': }
   ruby::version { '2.1.0': }
   ruby::version { '2.1.2': }
+  ruby::version { '2.1.8': }
+  ruby::version { '2.2.4': }
 
   # common, useful packages
   package {
